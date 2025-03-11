3dd33bc215213399a23bdd60e6a6439e14b0ec6f
felix <felix@butnoice.com>
2025-03-11 16:24:58 +0800

    feat: sort pr by update

--- src/action_test.go
+++ src/action_test.go
@@ -1,28 +1,30 @@
 package src
 
 import (
-	"bytes"
-	"context"
-	"git.andinfinity.de/gitea-release-drafter/src/config"
-	"testing"
+    "bytes"
+    "context"
+    "os"
+    "testing"
+
+    "git.andinfinity.de/gitea-release-drafter/src/config"
 )
 
 func Test_updateOrCreateDraftRelease(t *testing.T) {
-	cfg := &config.DrafterConfig{
-		ApiUrl:    "https://git.tianjinzhaofa.cn/",
-		Token:     "68b91e8988b6179f081ab1a488d606c8f2d8778c",
-		RepoOwner: "felix",
-		RepoName:  "butnoice",
-	}
-	ctx := context.WithValue(context.Background(), "key", "value")
-	a, err := NewAction(&ctx, cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	configReader := bytes.NewReader([]byte{})
-	repoCfg, err := config.ReadRepoConfig(configReader, "main")
-	if err != nil {
-		panic(err)
-	}
-	updateOrCreateDraftRelease(a, repoCfg)
+    cfg := &config.DrafterConfig{
+        ApiUrl:    "https://git.tianjinzhaofa.cn/",
+        Token:     os.Getenv("GITEA_ACCESS_TOKEN"),
+        RepoOwner: "braid2048",
+        RepoName:  "wizard",
+    }
+    ctx := context.WithValue(context.Background(), "key", "value")
+    a, err := NewAction(&ctx, cfg)
+    if err != nil {
+        t.Fatal(err)
+    }
+    configReader := bytes.NewReader([]byte{})
+    repoCfg, err := config.ReadRepoConfig(configReader, "main")
+    if err != nil {
+        panic(err)
+    }
+    updateOrCreateDraftRelease(a, repoCfg)
 }
--- src/changelog.go
+++ src/changelog.go
@@ -1,73 +1,76 @@
 package src
 
 import (
-	"code.gitea.io/sdk/gitea"
-	"github.com/sethvargo/go-githubactions"
-	"regexp"
-	"strings"
+    "regexp"
+    "strings"
+
+    "code.gitea.io/sdk/gitea"
+    "github.com/sethvargo/go-githubactions"
 )
 
 type Changelog map[string][]*gitea.PullRequest
 
 // GenerateChangelog fetches all the pull requests merged into the default branch since the last release and groups them by label. note that duplicates might occur if a pull request has multiple labels.
 func GenerateChangelog(c *gitea.Client, owner string, repo string, lastRelease *gitea.Release) (*Changelog, error) {
-	changelogByLabels := make(Changelog)
+    changelogByLabels := make(Changelog)
 
-	// FIXME: use pagination
-	prs, _, err := c.ListRepoPullRequests(owner, repo, gitea.ListPullRequestsOptions{
-		State: gitea.StateClosed,
-	})
-	if err != nil {
-		return nil, err
-	}
+    // FIXME: use pagination
+    prs, _, err := c.ListRepoPullRequests(owner, repo, gitea.ListPullRequestsOptions{
+        State:       gitea.StateClosed,
+        Sort:        "recentupdate",
+        ListOptions: gitea.ListOptions{PageSize: 50},
+    })
+    if err != nil {
+        return nil, err
+    }
 
-	for _, pr := range prs {
-		// only consider merged pull requests. note that we can't filter by that in the API
-		if pr.HasMerged {
-			// if there was a release, only take into account pull requests that have been merged after that
-			if lastRelease == nil || lastRelease != nil && pr.Merged.After(lastRelease.CreatedAt) {
-				for _, l := range pr.Labels {
-					_, ok := changelogByLabels[l.Name]
+    for _, pr := range prs {
+        // only consider merged pull requests. note that we can't filter by that in the API
+        if pr.HasMerged {
+            // if there was a release, only take into account pull requests that have been merged after that
+            if lastRelease == nil || lastRelease != nil && pr.Merged.After(lastRelease.CreatedAt) {
+                for _, l := range pr.Labels {
+                    _, ok := changelogByLabels[l.Name]
 
-					if ok {
-						changelogByLabels[l.Name] = append(changelogByLabels[l.Name], pr)
-					} else {
-						changelogByLabels[l.Name] = []*gitea.PullRequest{pr}
-					}
-				}
+                    if ok {
+                        changelogByLabels[l.Name] = append(changelogByLabels[l.Name], pr)
+                    } else {
+                        changelogByLabels[l.Name] = []*gitea.PullRequest{pr}
+                    }
+                }
 
-				if len(pr.Labels) == 0 {
-					githubactions.Warningf("PR #%d doesn't have any labels, guess one", pr.ID)
-					label := readLabelFromTitle(pr.Title)
-					changelogByLabels[label] = append(changelogByLabels[label], pr)
-				}
-			}
-		}
-	}
+                if len(pr.Labels) == 0 {
+                    githubactions.Warningf("PR #%d doesn't have any labels, guess one", pr.ID)
+                    label := readLabelFromTitle(pr.Title)
+                    changelogByLabels[label] = append(changelogByLabels[label], pr)
+                }
+            }
+        }
+    }
 
-	return &changelogByLabels, nil
+    return &changelogByLabels, nil
 }
 
 // if title is formatted like "label: xxx" or "[label] xxx" we should return "label" otherwise "(uncategorized)"
 func readLabelFromTitle(title string) string {
-	title = strings.TrimSpace(title)
-	// Define the regex pattern to match "label: xxx" or "[label] xxx"
-	pattern := `^(([^]]+):|\[([^]]+)\])`
+    title = strings.TrimSpace(title)
+    // Define the regex pattern to match "label: xxx" or "[label] xxx"
+    pattern := `^(([^]]+):|\[([^]]+)\])`
 
-	// Compile the regex
-	re := regexp.MustCompile(pattern)
+    // Compile the regex
+    re := regexp.MustCompile(pattern)
 
-	// Find the matches
-	matches := re.FindStringSubmatch(title)
+    // Find the matches
+    matches := re.FindStringSubmatch(title)
 
-	// If matches are found, return the label
-	if len(matches) > 2 {
-		if matches[2] != "" {
-			return matches[2]
-		}
-		return matches[3]
-	}
+    // If matches are found, return the label
+    if len(matches) > 2 {
+        if matches[2] != "" {
+            return matches[2]
+        }
+        return matches[3]
+    }
 
-	// If no matches
-	return "(uncategorized)"
+    // If no matches
+    return "(uncategorized)"
 }
